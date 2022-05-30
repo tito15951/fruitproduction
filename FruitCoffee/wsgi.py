70564ed82cf8d615ba3f18474a2a1df31d010cb3
Javier Martinez <javier.martinez@fianly.com>
2022-05-30 15:45:35 -0500

    Cambios

--- FruitCoffee/wsgi.py
+++ FruitCoffee/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FruitCoffee.FruitCoffee.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FruitCoffee.settings')
 
 application = get_wsgi_application()
