ed00b9f9de7df589015431d9e65c61819da49e2e
Javier Martinez <javier.martinez@fianly.com>
2022-05-30 18:00:25 -0500

    Pruebas imagenes

--- FruitCoffee/settings.py
+++ FruitCoffee/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static- files/
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'')
-#MEDIA_URL='/imagenes/'
-MEDIA_URL='/static/'
+MEDIA_URL='/imagenes/'
+#MEDIA_URL='/static/'
 
 #MEDIA_URL = '/imagenes/'
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
