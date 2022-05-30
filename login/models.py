2bd5cb19637c460a1c1d3eaa31af0c620e231bcf
Javier Martinez <javier.martinez@fianly.com>
2022-05-30 16:55:18 -0500

    Prueba imagenes creadas

--- login/models.py
+++ login/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 class Foto(models.Model):
     id=models.BigAutoField(primary_key=True)
-    foto=models.ImageField(upload_to='imagenes/')
+    foto=models.ImageField(upload_to='static/')
