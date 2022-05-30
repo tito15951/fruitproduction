d0781875919ee7e843112f34f133aa9da0e38693
Javier Martinez <javier.martinez@fianly.com>
2022-05-30 17:12:17 -0500

    Pruebas

--- login/models.py
+++ login/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 class Foto(models.Model):
     id=models.BigAutoField(primary_key=True)
-    foto=models.ImageField(upload_to='static/imagenes/')
+    foto=models.ImageField(upload_to='https://drive.google.com/drive/folders/1LPpRutERanX66GTEiFCHiFWc7TmnqELl?usp=sharing')
+    #foto=models.ImageField(upload_to='static/imagenes/')
