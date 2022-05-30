2ae8a8d61241c7f4ca483e6aa0e4834ca13fb433
Javier Martinez <javier.martinez@fianly.com>
2022-05-30 17:07:12 -0500

    Prueba imagenes credaas

--- FruitCoffee/settings.py
+++ FruitCoffee/settings.py
@@ -155,6 +155,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'static'),
 )
+
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 #MEDIA_URL='/login/templates/static/image/'
 
