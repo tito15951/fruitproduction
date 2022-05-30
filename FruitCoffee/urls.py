934502655be0aa1ed224ffb95caaefdd3112f947
Javier Martinez <javier.martinez@fianly.com>
2022-05-30 17:31:38 -0500

    Cambios

--- FruitCoffee/urls.py
+++ FruitCoffee/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('login.urls')),
     path('',include('APIFruitCoffee.urls')),
-]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
